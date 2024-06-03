a6bf6bd442f394e3a3416c115a62311936e87b48
Sangkwon Lee <sangkwon26@gmail.com>
2024-06-03 23:53:05 +0900

    feat: RuntimeExceptionHandler 추가
    - RuntimeExceptionHandler의 경우 고려하지 못한 예외이기에 로그를 getStackTrace로 결정

--- src/main/java/site/travellaboratory/be/common/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/site/travellaboratory/be/common/exceptionhandler/GlobalExceptionHandler.java
@@ -1,22 +1,41 @@
 package site.travellaboratory.be.common.exceptionhandler;
 
 import java.nio.charset.StandardCharsets;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import site.travellaboratory.be.common.exception.BeApplicationException;
+import site.travellaboratory.be.common.exception.ErrorCodes;
 import site.travellaboratory.be.common.response.ApiResponse;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(value = {BeApplicationException.class})
+    @ExceptionHandler(value = { BeApplicationException.class })
     public ResponseEntity<ApiResponse<Void>> beApplicationExceptionHandler(
         BeApplicationException exception) {
+        log.error("Error occurs {}", exception.toString());
+
         final ApiResponse<Void> body = ApiResponse.fromErrorCodes(exception.getErrorCodes());
         final MediaType contentType = new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8);
         final HttpStatus status = exception.getStatus();
+
+        return ResponseEntity.status(status)
+            .contentType(contentType)
+            .body(body);
+    }
+
+    @ExceptionHandler(value = { RuntimeException.class })
+    public ResponseEntity<ApiResponse<Void>> RuntimeExceptionHandler(
+        RuntimeException exception) {
+        log.error("Internal Server Error occurs {}", exception.getStackTrace());
+
+        final ApiResponse<Void> body = ApiResponse.fromErrorCodes(ErrorCodes.INTERNAL_SERVER_ERROR);
+        final MediaType contentType = new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8);
+        final HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
         return ResponseEntity.status(status)
             .contentType(contentType)
