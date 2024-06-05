b8734ac50a89e0fe4160c28c0960eb3fdf6ee42a
Sangkwon Lee <sangkwon26@gmail.com>
2024-06-05 15:27:19 +0900

    feat: [config] AuthorizationInterceptor 설정 및 추가, 인증이 필요없는 URL 설정

---
+++ src/main/java/site/travellaboratory/be/config/WebConfig.java
@@ -0,0 +1,42 @@
+package site.travellaboratory.be.config;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import site.travellaboratory.be.interceptor.AuthorizationInterceptor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthorizationInterceptor authorizationInterceptor;
+
+    private final List<String> PASS_URL = List.of(
+        "/api/*/auth/login",
+        "/api/*/auth/join",
+        "/api/*/auth/nickname"
+    );
+
+    private final List<String> DEFAULT_EXCLUDE = List.of(
+        "/",
+        "favicon.ico",
+        "/error"
+    );
+
+    private final List<String> SWAGGER = List.of(
+        "/swagger-ui/index.html",
+        "/swagger-ui/**",
+        "/v3/api-docs/**"
+    );
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 인증 인터셉터 등록
+        registry.addInterceptor(authorizationInterceptor)
+            .excludePathPatterns(PASS_URL)
+            .excludePathPatterns(DEFAULT_EXCLUDE)
+            .excludePathPatterns(SWAGGER);
+    }
+}
