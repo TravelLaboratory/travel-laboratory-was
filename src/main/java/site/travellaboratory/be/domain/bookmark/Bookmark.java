0ef4c9ef374d0333745ce80245e07dedfb5ff335
yuseonjun <seonjun0906@gmail.com>
2024-06-09 14:49:50 +0900

    feat: 북마크 조회, 저장 기능 구현

---
+++ src/main/java/site/travellaboratory/be/domain/bookmark/Bookmark.java
@@ -0,0 +1,43 @@
+package site.travellaboratory.be.domain.bookmark;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.travellaboratory.be.domain.BaseEntity;
+import site.travellaboratory.be.domain.article.Article;
+import site.travellaboratory.be.domain.user.User;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "bookmark")
+public class Bookmark extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    public Bookmark(final User user, final Article article) {
+        this.user = user;
+        this.article = article;
+    }
+
+    public static Bookmark of(final User user, final Article article) {
+        return new Bookmark(user, article);
+    }
+}
---
+++ src/main/java/site/travellaboratory/be/service/BookmarkService.java
@@ -0,0 +1,35 @@
+package site.travellaboratory.be.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import site.travellaboratory.be.controller.dto.BookmarkResponse;
+import site.travellaboratory.be.domain.article.Article;
+import site.travellaboratory.be.domain.article.ArticleRepository;
+import site.travellaboratory.be.domain.bookmark.Bookmark;
+import site.travellaboratory.be.domain.bookmark.BookmarkRepository;
+import site.travellaboratory.be.domain.user.User;
+import site.travellaboratory.be.domain.user.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class BookmarkService {
+
+    private final BookmarkRepository bookmarkRepository;
+    private final UserRepository userRepository;
+    private final ArticleRepository articleRepository;
+
+    public Long saveBookmark(final Long userId, final Long articleId) {
+        final User user = userRepository.getById(userId);
+        final Article article = articleRepository.getById(articleId);
+        final Bookmark bookMark = Bookmark.of(user, article);
+        bookmarkRepository.save(bookMark);
+        return bookMark.getId();
+    }
+
+    public List<BookmarkResponse> findAllBookmarkByUser(final Long userId) {
+        final User user = userRepository.getById(userId);
+        final List<Bookmark> bookmarkByUser = bookmarkRepository.findBookmarkByUser(user);
+        return BookmarkResponse.from(bookmarkByUser);
+    }
+}
