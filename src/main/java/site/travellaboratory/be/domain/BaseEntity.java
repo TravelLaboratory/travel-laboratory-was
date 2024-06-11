86b0b4660b35f87f63d730fee03d855c44a207be
yuseonjun <seonjun0906@gmail.com>
2024-06-11 17:52:46 +0900

    feat: jpa 관련 설정들 추가

--- src/main/java/site/travellaboratory/be/domain/BaseEntity.java
+++ src/main/java/site/travellaboratory/be/domain/BaseEntity.java
@@ -1,12 +1,15 @@
 package site.travellaboratory.be.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PreUpdate;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class BaseEntity {
 
