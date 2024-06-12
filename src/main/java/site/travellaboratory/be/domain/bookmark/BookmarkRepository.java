e2b9358a801e1b3321631223f86e18eedf14c306
yuseonjun <seonjun0906@gmail.com>
2024-06-12 18:14:46 +0900

    feat: 북마크 저장, 내 북마크 조회 기능 구현

--- src/main/java/site/travellaboratory/be/domain/bookmark/BookmarkRepository.java
+++ src/main/java/site/travellaboratory/be/domain/bookmark/BookmarkRepository.java
@@ -2,9 +2,12 @@ package site.travellaboratory.be.domain.bookmark;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import site.travellaboratory.be.domain.article.Article;
 import site.travellaboratory.be.domain.user.entity.User;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    Bookmark findByArticleAndUser(final Article article, final User user);
 
     List<Bookmark> findBookmarkByUser(final User user);
 }
