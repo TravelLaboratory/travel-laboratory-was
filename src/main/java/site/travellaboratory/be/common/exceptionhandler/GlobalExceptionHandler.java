5c6a2e55110efdc61586b9f4b655538e4a344d5f
Sangkwon Lee <sangkwon26@gmail.com>
2024-06-15 02:05:21 +0900

    modified: [config] GlobalExceptionHandler - Profile 제거, application dev ddl-auto : create를 update로 수정
    - 개발의 편의성을 위해 이렇게 기본값을 두고 시작합니다.

--- src/main/java/site/travellaboratory/be/common/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/site/travellaboratory/be/common/exceptionhandler/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +16,6 @@ import site.travellaboratory.be.common.exception.BeApplicationException;
 import site.travellaboratory.be.common.exception.ErrorCodes;
 import site.travellaboratory.be.common.response.ApiErrorResponse;
 
-@Profile("prod")
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
