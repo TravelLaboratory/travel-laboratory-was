ba30d541ab6f99a4c37077f786bb648f21d510df
yuseonjun <seonjun0906@gmail.com>
2024-06-19 18:10:41 +0900

    refactor: 프로필 수정 username 삭제

--- src/main/java/site/travellaboratory/be/controller/user/dto/UserProfileResponse.java
+++ src/main/java/site/travellaboratory/be/controller/user/dto/UserProfileResponse.java
@@ -1,15 +1,13 @@
 package site.travellaboratory.be.controller.user.dto;
 
-
 import site.travellaboratory.be.domain.user.entity.User;
 
 public record UserProfileResponse(
         String profileImgUrl,
         String name,
-        String introduce,
         boolean isEditable
 ) {
     public static UserProfileResponse from(final User user, boolean isEditable) {
-        return new UserProfileResponse(user.getProfileImgUrl(), user.getNickname(), user.getIntroduce(), isEditable);
+        return new UserProfileResponse(user.getProfileImgUrl(), user.getNickname(), isEditable);
     }
 }
