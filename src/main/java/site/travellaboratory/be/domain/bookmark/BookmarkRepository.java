dc159ae738394f097e9d4777a754388c486eefdf
yuseonjun <seonjun0906@gmail.com>
2024-06-23 17:03:39 +0900

    refactor: 북마크 내역 리팩토링

--- src/main/java/site/travellaboratory/be/domain/bookmark/BookmarkRepository.java
+++ src/main/java/site/travellaboratory/be/domain/bookmark/BookmarkRepository.java
@@ -2,6 +2,8 @@ package site.travellaboratory.be.domain.bookmark;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,13 +16,12 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     Optional<Bookmark> findByArticleAndUser(final Article article, final User user);
 
-    List<Bookmark> findByUserAndStatus(final User user, final BookmarkStatus status);
+//    List<Bookmark> findByUserAndStatus(final User user, final BookmarkStatus status);
 
-    Optional<List<Bookmark>> findByUserAndStatusIn(final User user, final List<BookmarkStatus> Status);
+    Optional<Page<Bookmark>> findByUserAndStatusIn(final User user, final List<BookmarkStatus> Status, Pageable pageable);
 
     @Query("SELECT COUNT(b) FROM Bookmark b WHERE b.article.id = :articleId AND b.status = :status")
     Long countByArticleIdAndStatus(@Param("articleId") Long articleId, @Param("status") BookmarkStatus status);
 
     boolean existsByUserIdAndArticleIdAndStatus(Long loginId, Long articleId, BookmarkStatus active);
-
 }
