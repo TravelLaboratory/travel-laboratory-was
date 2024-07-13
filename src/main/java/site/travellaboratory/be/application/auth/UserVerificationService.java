587dabd16b2ce3fdb21d0b049f4c1ee52c4e36f8
Sangkwon Lee <sangkwon26@gmail.com>
2024-07-13 14:18:21 +0900

    refactor: idxx메서드 boolean 반환 (컨벤션)

--- src/main/java/site/travellaboratory/be/application/auth/UserVerificationService.java
+++ src/main/java/site/travellaboratory/be/application/auth/UserVerificationService.java
@@ -5,12 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import site.travellaboratory.be.common.exception.BeApplicationException;
 import site.travellaboratory.be.common.exception.ErrorCodes;
-import site.travellaboratory.be.infrastructure.domains.user.UserJpaRepository;
 import site.travellaboratory.be.domain.user.enums.UserStatus;
+import site.travellaboratory.be.infrastructure.domains.user.UserJpaRepository;
 import site.travellaboratory.be.presentation.auth.dto.userverification.UserNicknameRequest;
-import site.travellaboratory.be.presentation.auth.dto.userverification.UserNicknameResponse;
 import site.travellaboratory.be.presentation.auth.dto.userverification.UsernameRequest;
-import site.travellaboratory.be.presentation.auth.dto.userverification.UsernameResponse;
 
 @Service
 @RequiredArgsConstructor
@@ -18,23 +16,23 @@ public class UserVerificationService {
 
     private final UserJpaRepository userJpaRepository;
 
-    public UserNicknameResponse isNicknameAvailable(UserNicknameRequest request) {
+    public Boolean isNicknameAvailable(UserNicknameRequest request) {
         // 닉네임 중복 체크
         userJpaRepository.findByNickname(request.nickname()).ifPresent(it -> {
             throw new BeApplicationException(ErrorCodes.AUTH_DUPLICATED_NICK_NAME,
                 HttpStatus.CONFLICT);
         });
 
-        return UserNicknameResponse.from(true);
+        return true;
     }
 
-    public UsernameResponse isUsernameAvailable(final UsernameRequest request) {
+    public Boolean isUsernameAvailable(final UsernameRequest request) {
         // (회원아이디) 이메일 중복 체크
         userJpaRepository.findByUsernameAndStatusOrderByIdDesc(request.username(), UserStatus.ACTIVE).ifPresent(it -> {
             throw new BeApplicationException(ErrorCodes.AUTH_DUPLICATED_USER_NAME,
                 HttpStatus.CONFLICT);
         });
 
-        return UsernameResponse.from(true);
+        return true;
     }
 }
