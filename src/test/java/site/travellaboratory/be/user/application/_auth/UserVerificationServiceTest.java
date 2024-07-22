82441e75b12938b9a93bad8e85ae881a30738707
Sangkwon Lee <sangkwon26@gmail.com>
2024-07-22 12:31:31 +0900

    test: UserVerificationService가 userRepository를 의존하게함으로써 소형테스트로 수정

--- src/test/java/site/travellaboratory/be/user/application/_auth/UserVerificationServiceTest.java
+++ src/test/java/site/travellaboratory/be/user/application/_auth/UserVerificationServiceTest.java
@@ -2,44 +2,47 @@ package site.travellaboratory.be.user.application._auth;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
 
-import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import site.travellaboratory.be.common.exception.BeApplicationException;
 import site.travellaboratory.be.common.exception.ErrorCodes;
+import site.travellaboratory.be.mock.user.FakeUserRepository;
 import site.travellaboratory.be.user.domain.User;
 import site.travellaboratory.be.user.domain._auth.request.UserNicknameRequest;
 import site.travellaboratory.be.user.domain._auth.request.UsernameRequest;
 import site.travellaboratory.be.user.domain.enums.UserStatus;
-import site.travellaboratory.be.user.infrastructure.persistence.entity.UserEntity;
-import site.travellaboratory.be.user.infrastructure.persistence.repository.UserJpaRepository;
 
-@ExtendWith(MockitoExtension.class)
 class UserVerificationServiceTest {
 
-    @InjectMocks
     private UserVerificationService sut;
+    private FakeUserRepository fakeUserRepository;
 
-    @Mock
-    private UserJpaRepository userJpaRepository;
+    @BeforeEach
+    void setUp() {
+        fakeUserRepository = new FakeUserRepository();
+        this.sut = new UserVerificationService(fakeUserRepository);
+    }
 
     @Nested
     class isNicknameAvailable {
+
+        private final String existingNickname = "existing_nickname";
+        @BeforeEach
+        void setUp() {
+            fakeUserRepository.save(User.builder()
+                .nickname(existingNickname)
+                .build()
+            );
+        }
+
         @DisplayName("이미_존재하는_nickname_인_경우")
         @Test
         void test1() {
             //given
-            String existingNickname = "existing_nickname";
-            UserEntity userEntity = UserEntity.from(User.builder().nickname(existingNickname).build());
             UserNicknameRequest invalidRequest = UserNicknameRequest.builder().nickname(existingNickname).build();
-            when(userJpaRepository.findByNickname(existingNickname)).thenReturn(Optional.of(userEntity));
 
             //when & then
             assertThrows(BeApplicationException.class, () ->
@@ -53,7 +56,6 @@ class UserVerificationServiceTest {
             //given
             String newNickname = "new_nickname";
             UserNicknameRequest validRequest = UserNicknameRequest.builder().nickname(newNickname).build();
-            when(userJpaRepository.findByNickname(newNickname)).thenReturn(Optional.empty());
 
             //when & then
             Boolean result = sut.isNicknameAvailable(validRequest);
@@ -63,15 +65,22 @@ class UserVerificationServiceTest {
 
     @Nested
     class isUsernameAvailable {
+        private final String existingUsername = "existing_username";
+
+        @BeforeEach
+        void setUp() {
+            fakeUserRepository.save(User.builder()
+                .username(existingUsername)
+                .status(UserStatus.ACTIVE)
+                .build()
+            );
+        }
+
         @DisplayName("이미_존재하는_username_인_경우")
         @Test
         void test1() {
             //given
-            String existingUsername = "existing_username";
-            UserEntity userEntity = UserEntity.from(User.builder().username(existingUsername).build());
             UsernameRequest invalidRequest = UsernameRequest.builder().username(existingUsername).build();
-            when(userJpaRepository.findByUsernameAndStatusOrderByIdDesc(existingUsername, UserStatus.ACTIVE))
-                .thenReturn(Optional.of(userEntity));
 
             //when & then
             assertThrows(BeApplicationException.class, () ->
@@ -85,7 +94,6 @@ class UserVerificationServiceTest {
             //given
             String newUserName = "new_username";
             UsernameRequest validRequest = UsernameRequest.builder().username(newUserName).build();
-            when(userJpaRepository.findByUsernameAndStatusOrderByIdDesc(newUserName, UserStatus.ACTIVE)).thenReturn(Optional.empty());
 
             //when & then
             Boolean result = sut.isUsernameAvailable(validRequest);
