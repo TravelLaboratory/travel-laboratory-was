22b3fcab81f3b1264968ff37869d1b085b6cda8a
Sangkwon Lee <sangkwon26@gmail.com>
2024-07-25 14:58:55 +0900

    refactor: ReviewJpaRepository 인터페이스와 구현체를 분리하여 의존성 역전을 통해 JPA 의존성 끊기, ArticleRepository 현재 필요한 걸 제외하고 전부 제거

--- src/main/java/site/travellaboratory/be/article/infrastructure/persistence/repository/ArticleRepositoryImpl.java
+++ src/main/java/site/travellaboratory/be/article/infrastructure/persistence/repository/ArticleRepositoryImpl.java
@@ -3,15 +3,11 @@ package site.travellaboratory.be.article.infrastructure.persistence.repository;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import site.travellaboratory.be.article.application.port.ArticleRepository;
 import site.travellaboratory.be.article.domain.Article;
 import site.travellaboratory.be.article.domain.enums.ArticleStatus;
 import site.travellaboratory.be.article.infrastructure.persistence.entity.ArticleEntity;
-import site.travellaboratory.be.user.domain.User;
-import site.travellaboratory.be.user.infrastructure.persistence.entity.UserEntity;
 
 @Repository
 @RequiredArgsConstructor
@@ -22,42 +18,6 @@ public class ArticleRepositoryImpl implements ArticleRepository {
     @Override
     public Optional<Article> findByIdAndStatusIn(Long articleId, List<ArticleStatus> Status) {
         return articleJpaRepository.findByIdAndStatusIn(articleId, Status)
-            .map(ArticleEntity::toModel);
-    }
-
-    @Override
-    public List<Article> findAllByStatus(ArticleStatus status) {
-        return articleJpaRepository.findAllByStatus(status).stream()
-            .map(ArticleEntity::toModel).toList();
-    }
-
-    @Override
-    public Page<Article> findAllByStatusOrderByCreatedAtDesc(ArticleStatus status, Pageable pageable) {
-        return articleJpaRepository.findAllByStatusOrderByCreatedAtDesc(status, pageable)
-            .map(ArticleEntity::toModel);
-    }
-
-    @Override
-    public Optional<List<Article>> findByUserEntityAndStatusIn(User user, List<ArticleStatus> status) {
-        return articleJpaRepository.findByUserEntityAndStatusIn(UserEntity.from(user), status)
-            .map(entities -> entities.stream().map(ArticleEntity::toModel).toList());
-    }
-
-    @Override
-    public Optional<Page<Article>> findByUserEntityAndStatusIn(User user, List<ArticleStatus> status, Pageable pageable) {
-        return articleJpaRepository.findByUserEntityAndStatusIn(UserEntity.from(user), status, pageable)
-            .map(page -> page.map(ArticleEntity::toModel));
-    }
-
-    @Override
-    public List<Article> findByLocationCityContainingAndStatusActive(String keyword, ArticleStatus status) {
-        return articleJpaRepository.findByLocationCityContainingAndStatusActive(keyword, status).stream()
-            .map(ArticleEntity::toModel).toList();
-    }
-
-    @Override
-    public Page<Article> findByLocationCityContainingAndStatusActive(String keyword, Pageable pageable, ArticleStatus status) {
-        return articleJpaRepository.findByLocationCityContainingAndStatusActive(keyword, pageable, status)
             .map(ArticleEntity::toModel);
     }
 }
--- src/main/java/site/travellaboratory/be/review/application/service/ReviewWriterService.java
+++ src/main/java/site/travellaboratory/be/review/application/service/ReviewWriterService.java
@@ -5,95 +5,87 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import site.travellaboratory.be.common.exception.BeApplicationException;
-import site.travellaboratory.be.common.exception.ErrorCodes;
+import site.travellaboratory.be.article.application.port.ArticleRepository;
 import site.travellaboratory.be.article.domain.Article;
 import site.travellaboratory.be.article.domain.enums.ArticleStatus;
+import site.travellaboratory.be.common.exception.BeApplicationException;
+import site.travellaboratory.be.common.exception.ErrorCodes;
+import site.travellaboratory.be.review.application.port.ReviewRepository;
 import site.travellaboratory.be.review.domain.Review;
 import site.travellaboratory.be.review.domain.enums.ReviewStatus;
-import site.travellaboratory.be.user.domain.enums.UserStatus;
-import site.travellaboratory.be.user.domain.User;
-import site.travellaboratory.be.article.infrastructure.persistence.repository.ArticleJpaRepository;
-import site.travellaboratory.be.article.infrastructure.persistence.entity.ArticleEntity;
-import site.travellaboratory.be.review.infrastructure.persistence.entity.ReviewEntity;
-import site.travellaboratory.be.review.infrastructure.persistence.repository.ReviewJpaRepository;
-import site.travellaboratory.be.user.infrastructure.persistence.entity.UserEntity;
-import site.travellaboratory.be.user.infrastructure.persistence.repository.UserJpaRepository;
 import site.travellaboratory.be.review.domain.request.ReviewSaveRequest;
 import site.travellaboratory.be.review.domain.request.ReviewUpdateRequest;
+import site.travellaboratory.be.user.application.port.UserRepository;
+import site.travellaboratory.be.user.domain.User;
+import site.travellaboratory.be.user.domain.enums.UserStatus;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewWriterService {
 
-    private final ReviewJpaRepository reviewJpaRepository;
-    private final UserJpaRepository userJpaRepository;
-    private final ArticleJpaRepository articleJpaRepository;
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+    private final ArticleRepository articleRepository;
 
 
     @Transactional
     public Long save(Long userId, ReviewSaveRequest request) {
-        Article article = getArticleById(request.articleId()).toModel();
+        Article article = getArticleById(request.articleId());
         verifyExistReviewBy(article);
-        User user = getUserById(userId).toModel();
+        User user = getUserById(userId);
 
         // Review 클래스에서 검증하고 생성
         Review review = Review.create(user, article, request);
-        Review saveReview = reviewJpaRepository.save(ReviewEntity.from(review)).toModel();
+        Review saveReview = reviewRepository.save(review);
         return saveReview.getId();
     }
 
 
     @Transactional
     public Long update(Long userId, Long reviewId, ReviewUpdateRequest request) {
-        Review review = getReviewById(reviewId).toModel();
-        User user = getUserById(userId).toModel();
+        Review review = getReviewById(reviewId);
+        User user = getUserById(userId);
 
         // 후기 업데이트
         Review updatedReview = review.withUpdatedContent(user, request);
-        reviewJpaRepository.save(ReviewEntity.from(updatedReview)).toModel();
+        reviewRepository.save(updatedReview);
         return updatedReview.getId();
     }
 
     @Transactional
     public boolean delete(final Long userId, final Long reviewId) {
-        Review review = getReviewById(reviewId).toModel();
-        User user = getUserById(userId).toModel();
+        Review review = getReviewById(reviewId);
+        User user = getUserById(userId);
 
         // 후기 삭제
         Review deletedReview = review.withInactiveStatus(user);
-        Review result = reviewJpaRepository.save(ReviewEntity.from(deletedReview)).toModel();
+        Review result = reviewRepository.save(deletedReview);
         return result.getStatus() == ReviewStatus.INACTIVE;
     }
 
-    private ArticleEntity getArticleById(Long articleId) {
-        return articleJpaRepository.findByIdAndStatusIn(articleId,
-                List.of(ArticleStatus.ACTIVE, ArticleStatus.PRIVATE))
-            .orElseThrow(() -> new BeApplicationException(ErrorCodes.REVIEW_POST_INVALID,
-                HttpStatus.NOT_FOUND));
+    private Article getArticleById(Long articleId) {
+        return articleRepository.findByIdAndStatusIn(articleId, List.of(ArticleStatus.ACTIVE, ArticleStatus.PRIVATE))
+            .orElseThrow(() -> new BeApplicationException(ErrorCodes.REVIEW_INVALID_ARTICLE_ID, HttpStatus.NOT_FOUND));
     }
 
-    private UserEntity getUserById(Long userId) {
-        return userJpaRepository.findByIdAndStatus(userId, UserStatus.ACTIVE)
-            .orElseThrow(
-                () -> new BeApplicationException(ErrorCodes.USER_NOT_FOUND, HttpStatus.NOT_FOUND));
+    private User getUserById(Long userId) {
+        return userRepository.getByIdAndStatus(userId, UserStatus.ACTIVE);
     }
 
-    private ReviewEntity getReviewById(Long reviewId) {
-        return reviewJpaRepository.findByIdAndStatusIn(reviewId,
-                List.of(ReviewStatus.ACTIVE, ReviewStatus.PRIVATE))
+    private Review getReviewById(Long reviewId) {
+        return reviewRepository.findByIdAndStatusIn(reviewId, List.of(ReviewStatus.ACTIVE, ReviewStatus.PRIVATE))
             .orElseThrow(() -> new BeApplicationException(ErrorCodes.REVIEW_UPDATE_INVALID,
                 HttpStatus.NOT_FOUND));
     }
 
     // 이미 해당 여행 계획에 대한 후기가 있을 경우
     private void verifyExistReviewBy(Article article) {
-        reviewJpaRepository.findByArticleEntityAndStatusInOrderByArticleEntityDesc(
-                ArticleEntity.from(article),
+        reviewRepository.findByArticleAndStatusInOrderByArticleDesc(article,
                 List.of(ReviewStatus.ACTIVE, ReviewStatus.PRIVATE))
-            .ifPresent(it -> {
-                throw new BeApplicationException(ErrorCodes.REVIEW_POST_EXIST,
-                    HttpStatus.CONFLICT);
-            });
+            .ifPresent(
+                it -> {
+                    throw new BeApplicationException(ErrorCodes.REVIEW_POST_EXIST,
+                        HttpStatus.CONFLICT);
+                });
     }
 }
