d04b22b9c8704f3771c0bf14c2a29efb92f63819
Sangkwon Lee <sangkwon26@gmail.com>
2024-07-29 20:50:50 +0900

    refactor: CommentLikeService - JpaRepository 의존성 역전 가능한 레포지토리 의존성 끊기

--- src/main/java/site/travellaboratory/be/comment/application/service/CommentLikeService.java
+++ src/main/java/site/travellaboratory/be/comment/application/service/CommentLikeService.java
@@ -5,51 +5,57 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import site.travellaboratory.be.common.exception.BeApplicationException;
-import site.travellaboratory.be.common.exception.ErrorCodes;
+import site.travellaboratory.be.comment.application.port.CommentRepository;
 import site.travellaboratory.be.comment.domain.Comment;
 import site.travellaboratory.be.comment.domain.CommentLike;
 import site.travellaboratory.be.comment.domain.enums.CommentLikeStatus;
-import site.travellaboratory.be.comment.infrastructure.persistence.repository.CommentJpaRepository;
 import site.travellaboratory.be.comment.domain.enums.CommentStatus;
-import site.travellaboratory.be.user.infrastructure.persistence.repository.UserJpaRepository;
-import site.travellaboratory.be.user.infrastructure.persistence.entity.UserEntity;
-import site.travellaboratory.be.comment.infrastructure.persistence.repository.CommentLikeJpaRepository;
 import site.travellaboratory.be.comment.infrastructure.persistence.entity.CommentLikeEntity;
+import site.travellaboratory.be.comment.infrastructure.persistence.repository.CommentLikeJpaRepository;
+import site.travellaboratory.be.common.exception.BeApplicationException;
+import site.travellaboratory.be.common.exception.ErrorCodes;
+import site.travellaboratory.be.user.application.port.UserRepository;
+import site.travellaboratory.be.user.domain.User;
 import site.travellaboratory.be.user.domain.enums.UserStatus;
 
 @Service
 @RequiredArgsConstructor
 public class CommentLikeService {
 
-    private final CommentJpaRepository commentJpaRepository;
+    private final CommentRepository commentRepository;
     private final CommentLikeJpaRepository commentLikeJpaRepository;
-    private final UserJpaRepository userJpaRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public CommentLikeStatus toggleLike(Long userId, Long commentId) {
         // 유효하지 않은 댓글에 좋아요하려고 할 경우
-        Comment comment = commentJpaRepository.findByIdAndStatusIn(commentId,
-                List.of(CommentStatus.ACTIVE))
-            .orElseThrow(() -> new BeApplicationException(ErrorCodes.COMMENT_LIKE_INVALID,
-                HttpStatus.NOT_FOUND)).toModel();
+        Comment comment = getCommentById(commentId);
 
         CommentLikeEntity commentLikeEntity = commentLikeJpaRepository.findByUserIdAndCommentId(userId,
                 commentId)
             .orElse(null);
 
         // 좋아요 누른 유저 가져오기
-        UserEntity userEntity = userJpaRepository.findByIdAndStatus(userId, UserStatus.ACTIVE)
-            .orElseThrow(
-                () -> new BeApplicationException(ErrorCodes.USER_NOT_FOUND, HttpStatus.NOT_FOUND));
+        User user = getUserById(userId);
 
         CommentLike commentLike;
         if (commentLikeEntity != null) {
             commentLike = commentLikeEntity.toModel().withToggleStatus();
         } else {
-            commentLike = CommentLike.create(userEntity.toModel(), comment);
+            commentLike = CommentLike.create(user, comment);
         }
         CommentLikeEntity saveCommentLike = commentLikeJpaRepository.save(CommentLikeEntity.from(commentLike));
         return saveCommentLike.getStatus();
+    }
+
+    private User getUserById(Long userId) {
+        return userRepository.getByIdAndStatus(userId, UserStatus.ACTIVE);
+    }
+
+    private Comment getCommentById(Long commentId) {
+        return commentRepository.findByIdAndStatusIn(commentId,
+                List.of(CommentStatus.ACTIVE))
+            .orElseThrow(() -> new BeApplicationException(ErrorCodes.COMMENT_LIKE_INVALID,
+                HttpStatus.NOT_FOUND));
     }
 }
