034ab05d19e6b18f24db9986e0424a965986dae5
Sangkwon Lee <sangkwon26@gmail.com>
2024-08-22 12:57:22 +0900

    style: 불필요한 주석 제거

--- src/main/java/site/travellaboratory/be/comment/application/service/CommentReaderService.java
+++ src/main/java/site/travellaboratory/be/comment/application/service/CommentReaderService.java
@@ -9,23 +9,22 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import site.travellaboratory.be.common.exception.BeApplicationException;
-import site.travellaboratory.be.common.error.ErrorCodes;
-import site.travellaboratory.be.comment.infrastructure.persistence.repository.CommentJpaRepository;
-import site.travellaboratory.be.comment.infrastructure.persistence.entity.CommentEntity;
-import site.travellaboratory.be.comment.domain.enums.CommentStatus;
-import site.travellaboratory.be.review.infrastructure.persistence.repository.ReviewJpaRepository;
-import site.travellaboratory.be.review.infrastructure.persistence.entity.ReviewEntity;
-import site.travellaboratory.be.review.domain.enums.ReviewStatus;
-import site.travellaboratory.be.user.infrastructure.persistence.repository.UserJpaRepository;
-import site.travellaboratory.be.user.infrastructure.persistence.entity.UserEntity;
-import site.travellaboratory.be.user.domain.enums.UserStatus;
-import site.travellaboratory.be.comment.infrastructure.persistence.repository.CommentLikeJpaRepository;
-import site.travellaboratory.be.comment.infrastructure.persistence.entity.CommentLikeEntity;
 import site.travellaboratory.be.comment.domain.enums.CommentLikeStatus;
+import site.travellaboratory.be.comment.domain.enums.CommentStatus;
+import site.travellaboratory.be.comment.infrastructure.persistence.entity.CommentEntity;
+import site.travellaboratory.be.comment.infrastructure.persistence.entity.CommentLikeEntity;
+import site.travellaboratory.be.comment.infrastructure.persistence.repository.CommentJpaRepository;
+import site.travellaboratory.be.comment.infrastructure.persistence.repository.CommentLikeJpaRepository;
 import site.travellaboratory.be.comment.presentation.response.reader.CommentLikeCount;
 import site.travellaboratory.be.comment.presentation.response.reader.CommentReadPaginationResponse;
 import site.travellaboratory.be.comment.presentation.response.reader.CommentReadResponse;
+import site.travellaboratory.be.common.error.ErrorCodes;
+import site.travellaboratory.be.common.exception.BeApplicationException;
+import site.travellaboratory.be.review.domain.enums.ReviewStatus;
+import site.travellaboratory.be.review.infrastructure.persistence.repository.ReviewJpaRepository;
+import site.travellaboratory.be.user.domain.User;
+import site.travellaboratory.be.user.domain.enums.UserStatus;
+import site.travellaboratory.be.user.infrastructure.persistence.repository.UserJpaRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -41,14 +40,12 @@ public class CommentReaderService {
         Long userId, Long reviewId, int page, int size
     ) {
         // 유효하지 않은 후기를 조회할 경우
-        // todo: (1) review.getUser() 이 때 쿼리 날라감 (FETCH JOIN) - 55 review user fetch join
-        ReviewEntity reviewEntity = reviewJpaRepository.findByIdAndStatus(reviewId, ReviewStatus.ACTIVE)
-            .orElseThrow(
-                () -> new BeApplicationException(ErrorCodes.COMMENT_READ_ALL_PAGINATION_INVALID,
-                    HttpStatus.NOT_FOUND));
+        validateReview(reviewId);
+
+        // 유효한 유저인지 조회
+        User user = getUserById(userId);
 
         // 후기 리스트 조회 (N+1 쿼리 해결)
-        // todo: 62 ~ 82 단순 테이블안에서 변환 N+1 발생 X
         // (1) 페이징 처리
         Page<CommentEntity> commentPage = commentJpaRepository.findByReviewEntityIdAndStatusOrderByCreatedAtDesc(reviewId,
             CommentStatus.ACTIVE, PageRequest.of(page, size));
@@ -72,7 +69,6 @@ public class CommentReaderService {
             ));
 
         // (4) userId와 commentId 들를 기준으로 데이터 리스트 뽑아오기 (N+1 해결)
-        // todo: before after 비교해보기
         List<CommentLikeEntity> commentLikeJpaEntities = commentLikeJpaRepository.findAllByUserIdAndCommentIdInAndStatusFetchJoinComment(
             userId, commentIds,
             CommentLikeStatus.ACTIVE);
@@ -83,16 +79,10 @@ public class CommentReaderService {
                 CommentLikeEntity::getStatus
             ));
 
-        // todo : test - FETCH JOIN으로 N+1 해결하기 (먼저 commentId에 관련된 User 가져오기)
         // (4) 댓글과 연관된 유저 정보를 포함한 응답 생성
-        // commentPage.getContent() 에서 commentIds 를 이용해 user 를 fetch join 으로 로딩
-        // todo : after N+1 해결
         List<CommentEntity> commentsWithUsers = commentJpaRepository.findAllByIdInFetchJoinUser(commentIds, CommentStatus.ACTIVE);
 
         // (5) 최종 데이터 반환
-        // todo : before N+1 문제
-//        List<CommentReadResponse> comments = commentPage.stream().map(comment -> {
-            // todo : after N+1 해결
         List<CommentReadResponse> comments = commentsWithUsers.stream().map(comment -> {
                 // isLikes 에서 commentId 가 있고, 그게 ACTIVE 라면 true
                 boolean isLike = isLikes.containsKey(comment.getId())
@@ -108,11 +98,20 @@ public class CommentReaderService {
             }
         ).toList();
 
-        // (댓글을 쓰는 유저 조회) todo: refactoring
-        UserEntity userEntity = userJpaRepository.findByIdAndStatus(userId, UserStatus.ACTIVE)
-            .orElseThrow(
-                () -> new BeApplicationException(ErrorCodes.USER_NOT_FOUND, HttpStatus.NOT_FOUND));
+        return CommentReadPaginationResponse.from(user.getProfileImgUrl(), comments, commentPage);
 
-        return CommentReadPaginationResponse.from(userEntity.getProfileImgUrl(), comments, commentPage);
+    }
+    private void validateReview(Long reviewId) {
+        reviewJpaRepository.findByIdAndStatus(reviewId, ReviewStatus.ACTIVE)
+            .orElseThrow(
+                () -> new BeApplicationException(ErrorCodes.COMMENT_READ_ALL_PAGINATION_INVALID,
+                    HttpStatus.NOT_FOUND)).toModel();
+    }
+
+    private User getUserById(Long userId) {
+        return userJpaRepository.findByIdAndStatus(userId, UserStatus.ACTIVE)
+            .orElseThrow(
+                () -> new BeApplicationException(ErrorCodes.USER_NOT_FOUND, HttpStatus.NOT_FOUND))
+            .toModel();
     }
 }
