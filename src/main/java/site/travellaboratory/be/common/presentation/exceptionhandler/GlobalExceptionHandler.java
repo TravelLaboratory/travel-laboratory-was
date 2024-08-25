c8a54d366fedb0ef378ecc88803d4ce8bf42d0b6
Sangkwon Lee <sangkwon26@gmail.com>
2024-08-25 23:42:00 +0900

    tmp: Profile dev로 변경 - 에러 원인 분석을 위해

--- src/main/java/site/travellaboratory/be/common/presentation/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/site/travellaboratory/be/common/presentation/exceptionhandler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import site.travellaboratory.be.common.error.ErrorCodes;
 import site.travellaboratory.be.common.presentation.response.ApiResponse;
 
-//@Profile("prod")
+@Profile("dev")
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
