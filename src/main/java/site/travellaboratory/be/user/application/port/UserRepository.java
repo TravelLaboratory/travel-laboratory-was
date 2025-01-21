c604ddf0c42fe21e046673b814e0b450e53d5cc3
Sangkwon Lee <sangkwon26@gmail.com>
2025-01-21 14:46:50 +0900

    fix: 사용하지 않은 메서드 제거

--- src/main/java/site/travellaboratory/be/user/application/port/UserRepository.java
+++ src/main/java/site/travellaboratory/be/user/application/port/UserRepository.java
@@ -5,7 +5,6 @@ import site.travellaboratory.be.user.domain.User;
 import site.travellaboratory.be.user.domain.enums.UserStatus;
 
 public interface UserRepository {
-    User findByUserId(Long userId);
     User getByIdAndStatus(Long userId, UserStatus status);
     Optional<User> findByUsernameAndStatusOrderByIdDesc(String username, UserStatus status);
     Optional<User> findByNickname(String nickname);
--- src/main/java/site/travellaboratory/be/user/infrastructure/persistence/repository/UserRepositoryImpl.java
+++ src/main/java/site/travellaboratory/be/user/infrastructure/persistence/repository/UserRepositoryImpl.java
@@ -18,13 +18,6 @@ public class UserRepositoryImpl implements UserRepository {
     private final UserJpaRepository userJpaRepository;
 
     @Override
-    public User findByUserId(Long userId) {
-        return userJpaRepository.findById(userId).orElseThrow(
-                () -> new BeApplicationException(ErrorCodes.USER_NOT_FOUND, HttpStatus.NOT_FOUND))
-            .toModel();
-    }
-
-    @Override
     public User getByIdAndStatus(Long userId, UserStatus status) {
         return userJpaRepository.findByIdAndStatus(userId, status)
             .orElseThrow(() -> new BeApplicationException(ErrorCodes.USER_NOT_FOUND, HttpStatus.NOT_FOUND))
