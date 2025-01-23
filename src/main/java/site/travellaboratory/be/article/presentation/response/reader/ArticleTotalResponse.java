760bda37eac29432a8698bcfdfa2d008997659ff
k9want <zndwlrnjr26@daum.net>
2025-01-23 17:40:05 +0900

    update: name -> nickname으로 변경 및 userId 추가

--- src/main/java/site/travellaboratory/be/article/presentation/response/reader/ArticleTotalResponse.java
+++ src/main/java/site/travellaboratory/be/article/presentation/response/reader/ArticleTotalResponse.java
@@ -3,25 +3,27 @@ package site.travellaboratory.be.article.presentation.response.reader;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import site.travellaboratory.be.article.domain.enums.TravelStyle;
 import site.travellaboratory.be.article.infrastructure.persistence.entity.ArticleEntity;
 import site.travellaboratory.be.article.infrastructure.persistence.entity.ArticleLocationEntity;
 
 public record ArticleTotalResponse(
-    Long articleId,
-    String title,
-    List<ArticleLocationEntity> locations,
-    LocalDate startAt,
-    LocalDate endAt,
-    String expense,
-    String profileImgUrl,
-    String coverImgUrl,
-    String travelCompanion,
-    List<String> travelStyles,
-    String name,
-    Long bookmarkCount,
-    Boolean isBookmarked,
-    Boolean isEditable
+        Long userId, // 글쓴이 ID
+        String nickname, // 글쓴이 닉네임닉네임
+        String profileImgUrl,
+        Long articleId,
+        String title,
+        List<ArticleLocationEntity> locations,
+        LocalDate startAt,
+        LocalDate endAt,
+        String expense,
+        String coverImgUrl,
+        String travelCompanion,
+        List<String> travelStyles,
+        Long bookmarkCount,
+        Boolean isBookmarked,
+        Boolean isEditable
 
 ) {
     public static ArticleTotalResponse of(
@@ -35,17 +37,18 @@ public record ArticleTotalResponse(
                 .collect(Collectors.toList());
 
         return new ArticleTotalResponse(
+                articleEntity.getUserEntity().getId(),
+                articleEntity.getUserEntity().getNickname(),
+                articleEntity.getUserEntity().getProfileImgUrl(),
                 articleEntity.getId(),
                 articleEntity.getTitle(),
                 articleEntity.getLocationEntities(),
                 articleEntity.getStartAt(),
                 articleEntity.getEndAt(),
                 articleEntity.getExpense(),
-                articleEntity.getUserEntity().getProfileImgUrl(),
                 articleEntity.getCoverImgUrl(),
                 articleEntity.getTravelCompanion().getName(),
                 travelStyleNames,
-                articleEntity.getUserEntity().getNickname(),
                 bookmarkCount,
                 isBookmarked,
                 isEditable
